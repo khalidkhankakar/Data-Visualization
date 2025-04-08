63f1d19fb8635e49243bd794b8c32fae0787f692
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-08 23:17:38 +0500

    basics of line plot

---
+++ Basics/01_line_plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [1, 4, 9, 16]
+
+# line plot
+# plt.plot(x, y)
+# plt.title("Square Numbers")
+# plt.xlabel("Value")
+# plt.ylabel("Square of Value")
+# plt.show()
+
+plt.plot(x, y,'go',marker='+',
+         mouseover=True,
+           markersize=10, 
+           linewidth=5, 
+           linestyle=':', 
+           color='green',
+           fillstyle='top')
+plt.title("Square Numbers")
+plt.xlabel("Value")
+plt.ylabel("Square of Value")
+plt.show()
+plt.savefig('line_plot.png')
