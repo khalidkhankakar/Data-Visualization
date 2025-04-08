a96eb10c911d9700e40fdf231eaec8ec71a29ae6
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-08 23:24:58 +0500

    draw the multiple lines

---
+++ Basics/02_multiple_lines.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+# Data for Line 1
+x = [1, 2, 3, 4, 5]
+y1 = [1, 4, 9, 16, 25]
+
+# Data for Line 2
+y2 = [2, 3, 5, 7, 11]
+
+# Plot both lines
+# method1
+plt.plot(x, y1, label="Squares", color="blue", marker='o')
+plt.plot(x, y2, label="Primes", color="green", linestyle='--', marker='x')
+
+# method2
+# plt.plot(x,y1, 'r--', x,y2, 'b:')
+
+# Add labels and title
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.title("Multiple Lines Example")
+
+# Show legend
+plt.legend()
+
+# Show the plot
+plt.show()
