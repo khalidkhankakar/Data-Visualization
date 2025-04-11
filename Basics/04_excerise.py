d5d8c08c244125130b2f3081d7faf34951230de1
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-11 21:27:30 +0500

    modify x label values

--- Basics/04_excerise.py
+++ Basics/04_excerise.py
@@ -12,7 +12,7 @@ plt.plot(x, x3, 'go-', label='y = x³')       # Green line with circles
 # plt.plot(x, x, 'r--', x,x2, 'b:', x,x3, 'o')
 
 # Custom y-axis ticks
-plt.xticks([0,1,2,3,4,5,6], ['Zero','One', 'Two', 'Three','Four', 'Five', 'Six'])
+plt.xticks([0,1,2,3,4,5,6], ['Zero','One', 'Two', 'Three','Four', 'Five', 'Six','Seven'])
 
 plt.title('Excerise')
 plt.xlabel('X Axis')
